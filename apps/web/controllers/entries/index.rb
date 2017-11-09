21c47fad051df862b1d82470db104a63a13975db
yumidev <yumidev@users.noreply.github.com>
2017-11-09 20:24:58 +0900

    use sort params to sort my entries

--- apps/web/controllers/entries/index.rb
+++ apps/web/controllers/entries/index.rb
@@ -7,8 +7,16 @@ module Web::Controllers::Entries
 
     def call(params)
       # @entries = EntryRepository.new.all
-      @entries = EntryRepository.new.most_recent
       @sort = params[:sort]
+      
+      # most_recent or least_recent
+      
+      # TODO: Clean this up
+      if @sort == 'most_recent'
+       @entries = EntryRepository.new.most_recent
+      else
+       @entries = EntryRepository.new.least_recent
+      end
     end
   end
 end
